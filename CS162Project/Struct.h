3563bb72596a037f0aefd10912c9c377882010ad
JasonMdoesCPP <huuthinh711xd@gmail.com>
2024-03-13 15:54:46 +0700

    Creating main project and struct file

---
+++ CS162Project/Struct.h
@@ -0,0 +1,106 @@
+#ifndef STRUCT_H_INCLUDED
+#define STRUCT_H_INCLUDED
+
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+struct DateOfBirth{
+    int day;
+    int year;
+    int month;
+};
+
+/*Add a course to this semester: course id, course name, class name, teacher name, number of credits, the maximum number of students in the course (default 50), day of the week, and the session that the course will be performed (MON / TUE / WED / THU / FRI / SAT, S1 (07:30), S2 (09:30), S3(13:30) and S4 (15:30)). A course will be taught in only one session in a week.
+ */
+struct Score{
+    double totalMark;
+    double finalMark;
+    double midtermMark;
+    double otherMark;
+};
+
+struct StudentEnrolled{
+    StudentEnrolled* next;
+    string type; //20CLC1…, class 20CLC11, class 20VP…
+    int No;
+    string studentId;
+    string socialId;
+    string fristName;
+    string lastName;
+    string gender;
+    DateOfBirth dateOfBirth;
+    Score score;
+};
+
+struct CourseOfStudent{
+    CourseOfStudent*next;
+    string name; // View a list of his/her courses. He/she will study these courses in this semester.
+    int semester;
+    Score score;
+};
+
+//Then he/she will upload a CSV file, containing a list of students enrolled in the course.
+struct Course{
+    Course* next;
+    StudentEnrolled *studentEnrolled;
+    string id;
+    string name;
+    string teacherName;
+    int numberOfCredits;
+    int maxSize = 50;
+    char dow ;// day of week
+    int session;
+};
+
+struct Semester{
+    int startDay;
+    int endDay;
+    Course *course;
+};
+
+struct SchoolYear{
+    SchoolYear * next;
+    Semester semester;
+    int yearStart;
+    int yearEnd = yearStart+1;
+};
+
+struct Student{
+    Student *next;
+    CourseOfStudent courses;
+    string type; //20CLC1…, class 20CLC11, class 20VP…
+    int No;
+    string studentId;
+    string socialId;
+    string fristName;
+    string lastName;
+    string gender;
+    int schoolYear;
+    DateOfBirth dateOfBirth;
+};
+
+struct StaffMember{
+    StaffMember*next;
+    string firstName;
+    string lastName;
+    SchoolYear schoolYear;
+    void createSchoolYear();
+    void createSeveralClasses();
+};
+
+struct Class{
+    Class* next;
+    Student *student;
+};
+
+struct User{
+    StaffMember *staffMembers;
+    Student *students;
+    string passWord;
+    string userName;
+
+};
+
+#endif // STRUCT_H_INCLUDED
