212ce3f5ef9d4ae56d6eb8b68df5ec5a33308eb5
Uyhuynh23 <htuy23@apcs.fitus.edu.vn>
2024-03-25 14:53:25 +0700

    Check function 2

--- CS162Project/main.cpp
+++ CS162Project/main.cpp
@@ -1,9 +1,37 @@
-#include <iostream>
-#include "Struct.h"
-#include <string>
-using namespace std;
-
+#include"Struct.h"
 int main()
 {
-    User user; 
+	User *Head_User = new User;
+	Class* Head_Class = nullptr;
+	if (Head_User->user1) cout << "Sth wrong in user 1";
+	Head_User->addInformationStaffMembers();
+	Head_User->addInformationStudent();
+	Head_User->login();
+	if (Head_User->user1)
+	{
+
+	}
+	if (Head_User->user2)
+	{
+
+		int choice = 0;
+		while (choice != 6)
+		{
+			cout << "Enter choices:";
+			cout << " 1.Create school year." << endl;
+			cout << "2.Create several Classes." << endl;
+			cin >> choice;
+			switch (choice)
+			{
+			case 1:
+				Head_User->user2->createSchoolYear();
+				break;
+
+			case 2:
+				createSeveralClasses(Head_Class);
+				break;
+			}
+		}
+		cout << "fin";
+	}
 }
