ad8e1ff26eab7dab94f0fb443156dbd800cc40ea
Uyhuynh23 <htuy23@apcs.fitus.edu.vn>
2024-04-28 21:26:54 +0700

    Tạo student.csv, staffmemver.csv vào folder

--- CS162Project/Struct.cpp
+++ CS162Project/Struct.cpp
@@ -358,7 +358,7 @@ void importScoreBoard(Student* stu, Semester* semester,string CourseId)
 }
 void UpdateUser(User user){
     ofstream fout;
-    fout.open("student.csv", ios::trunc);
+    fout.open("User/student.csv", ios::trunc);
     if(!fout.is_open()){
         cout << "Error update file" << endl;
         return;
@@ -382,7 +382,7 @@ void UpdateUser(User user){
     }
     fout.close();
 
-    fout.open("staffMember.csv", ios::trunc);
+    fout.open("User/staffMember.csv", ios::trunc);
     if(!fout.is_open()){
         cout << "Error update file" << endl;
         return;
